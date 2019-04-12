25817337f14a09780c5a1952bc122a8189b7b8cd
vipslord <my.yershov@gmail.com>
2019-04-13 01:41:30 +0300

    upd header - rendering title for current day, hiding button today

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,14 +10,69 @@ class App extends Component {
   calendarComponentRef = React.createRef()
   state = {
     defaultView: config.calendar.defaultView,
-    active: false
+    active: false,
+    calendarDate: '',
+    view: '',
+    todayBtn: true
   }
-  componentDidMount() {
+  componentDidMount () {
     let calendarApi = this.calendarComponentRef.current.getApi()
     let activeWorker = config.workers.find(worker => worker.id == config.activeWorkerId)
     let businessHours = activeWorker.businessHours
-    calendarApi.setOption('businessHours', activeWorker.businessHours)
+    calendarApi.setOption('businessHours', businessHours)
     this.props.dispatch(getEvents())
+    this.getCalendarDate()
+  }
+  renderTime = title => {
+    let calendarApi = this.calendarComponentRef.current.getApi()
+    let currentDay = moment().format('YYYY-MM-DD') === moment(title).format('YYYY-MM-DD')
+    let currentMonth = moment().format('MM') === moment(title).format('MM')
+    let curDay = moment().format('YYYY-MM-DD')
+    let start = moment(calendarApi.state.dateProfile.currentRange.start).format('YYYY-MM-DD')
+    let end = moment(calendarApi.state.dateProfile.currentRange.end).format('YYYY-MM-DD')
+    let isBetween = moment(curDay).isBetween(moment(start), moment(end))
+    if (calendarApi.view.type === 'timeGridDay') {
+      this.setState({ view: config.translations.daily, todayBtn: currentDay ? false : true })
+      return (
+        <React.Fragment>
+          {currentDay && <span className='today'>{config.translations.today}</span>}
+          <span className={'current_date_field' + (currentDay ? ' for_today' : '')}>{moment(title).format('ddd, MMM DD')}</span>
+        </React.Fragment>
+      )
+    } else if (calendarApi.view.type === 'timeGridWeek') {
+      this.setState({ view: config.translations.weekly, todayBtn: isBetween ? false : true })
+      return (
+        <React.Fragment>
+          <span className='current_date_field' style={{ 'direction': 'ltr' }}>{moment(start).format('DD') + ' - ' + moment(end).subtract(1, 'days').format('DD') + ' ' + moment(start).format('MMM')}</span>
+          {isBetween && <span className='this_week'>{config.translations.thisWeek}</span>}
+        </React.Fragment>
+      )
+    } else if (calendarApi.view.type === 'dayGridMonth') {
+      this.setState({ view: config.translations.monthly, todayBtn: currentMonth ? false : true })
+      return (
+        <React.Fragment>
+          <span className='current_date_field'>{moment(title).format('MMMM')}</span>
+          <span className='this_week'>{moment(title).format('YYYY')}</span>
+        </React.Fragment>
+      )
+    } else if (calendarApi.view.type === 'listWeek') {
+      this.setState({ view: config.translations.agenda, todayBtn: currentDay ? false : true })
+      return (
+        <React.Fragment>
+          {currentDay && <span className='today'>{config.translations.today}</span>}
+          <span className={'current_date_field' + (currentDay ? ' for_today' : '')}>{moment(title).format('ddd, MMM DD')}</span>
+        </React.Fragment>
+      )
+    }
+  }
+
+  getCalendarDate = () => {
+    let calendarApi = this.calendarComponentRef.current.getApi()
+    let date = moment(calendarApi.getDate()).format('YYYY-MM-DD')
+    this.setState({
+      calendarDate: this.renderTime(date),
+      notFormattedDate: date
+    })
   }
   handleChangeView = () => {
     let calendarApi = this.calendarComponentRef.current.getApi()
@@ -30,18 +85,22 @@ class App extends Component {
     } else if (calendarApi.view.type === 'listWeek') {
       calendarApi.changeView('timeGridDay')
     }
+    this.getCalendarDate()
   }
   handleToday = () => {
     let calendarApi = this.calendarComponentRef.current.getApi()
     calendarApi.today()
+    this.getCalendarDate()
   }
   handleNext = () => {
     let calendarApi = this.calendarComponentRef.current.getApi()
     calendarApi.next()
+    this.getCalendarDate()
   }
   handlePrev = () => {
     let calendarApi = this.calendarComponentRef.current.getApi()
     calendarApi.prev()
+    this.getCalendarDate()
   }
   handleChangeWorker = id => {
     config.activeWorkerId = id
@@ -49,16 +108,25 @@ class App extends Component {
     let calendarApi = this.calendarComponentRef.current.getApi()
     calendarApi.setOption('businessHours', chosenWorker.businessHours)
     this.setState({ active: true })
+    this.getCalendarDate()
   }
   render () {
+    // console.log(this.state.calendarDate)
     return (
       <div className='app'>
-        <Header changeView={this.handleChangeView} today={this.handleToday} next={this.handleNext} prev={this.handlePrev} />
+        <Header
+          todayBtn={this.state.todayBtn}
+          changeView={this.handleChangeView}
+          title={this.state.calendarDate}
+          today={this.handleToday}
+          view={this.state.view}
+          next={this.handleNext}
+          prev={this.handlePrev} />
         <Workers changeWorker={this.handleChangeWorker} />
         <div id='swiper-calendar'>
           {/* <Swiper initialSlide={1} > */}
             {/* <div> */}
-              <DemoCalendar defaultView={this.state.defaultView} events={this.props.events} refName={this.calendarComponentRef} />
+            <DemoCalendar defaultView={this.state.defaultView} events={this.props.events} refName={this.calendarComponentRef} />
             {/* </div> */}
             {/* <div> */}
               {/* <DemoCalendar defaultView={this.state.defaultView} events={this.props.events} refName={this.calendarComponentRef} /> */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,41 @@
 import React, { Component } from 'react'
 import './Header.styl'
 
-class Header extends Component {
+export default class Header extends Component {
   render () {
+    const today = this.props.todayBtn
     return (
       <div id='header' style={config.calendar.isRTL ? {'direction': 'rtl'} : {'direction': 'ltr'}}>
         <div className={'menu_refresh ' + (config.calendar.isRTL ? 'menu_rtl' : 'menu_ltr')}>
           <button className='more_wrap'>
-            <img src={`${config.urls.staticImg}/ic_menu.svg`}/>
+            <img src={config.urls.staticImg + '/ic_menu.svg'} />
           </button>
           <button id='refresh_button'>
-            <img className='refresh_button_img' src={`${config.urls.staticImg}/refresh.svg`} />
+            <img className='refresh_button_img' src={config.urls.staticImg + '/refresh.svg'} />
           </button>
         </div>
         <div className='middle-section'>
           <button onClick={this.props.prev} className={'prev_button_wrap common ' + (config.calendar.isRTL ? 'rtlStyle' : 'ltrStyle')}>
-            <img className='prev_button btn' src={`${config.urls.staticImg}/prev.svg`} />
+            <img className='prev_button btn' src={config.urls.staticImg + '/prev.svg'} />
           </button>
           <div className='current_date'>
-            <span className='today'></span>
-            <span className='current_date_field'></span>
-            <span className='this_week'></span>
+            {this.props.title}
           </div>
           <button onClick={this.props.next} className={'next_button_wrap common ' + (config.calendar.isRTL ? 'rtlStyle' : 'ltrStyle')}>
-            <img className='next_button btn' src={`${config.urls.staticImg}/next.svg`} />
+            <img className='next_button btn' src={config.urls.staticImg + '/next.svg'} />
           </button>
         </div>
         <div className={'header_right ' + (config.calendar.isRTL ? 'view_buttons_rtl' : 'view_buttons_ltr')}>
-          <button className='today_wrap' onClick={this.props.today}>
-            <img className='img_today' src={`${config.urls.staticImg}/today.svg`} />
+          {today && <button className='today_wrap' onClick={this.props.today}>
+            <img className='img_today' src={config.urls.staticImg + '/today.svg'} />
             {config.translations.today}
-          </button>
+          </button>}
           <button className='daily_wrap daily_wrap_label' onClick={this.props.changeView}>
-            <img className='img_view' src={`${config.urls.staticImg}/change_view.svg`} />
-            {config.translations.agenda}
+            <img className='img_view' src={config.urls.staticImg + '/change_view.svg'} />
+            {this.props.view}
           </button>
         </div>
       </div>
     )
   }
 }
-export default Header
