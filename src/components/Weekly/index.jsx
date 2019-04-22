96c72579850462b5113e13fd7fca0b13587dd13f
vipslord <my.yershov@gmail.com>
2019-04-22 20:14:36 +0300

    updated weekly

--- src/components/Weekly/index.jsx
+++ src/components/Weekly/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import DemoCalendar from './DemoCalendar/index.jsx'
-import { getEvents } from 'store/events/actions'
+// import { getEvents } from 'store/events/actions'
 import CalendarModal from './CalendarModal/CalendarModal.jsx'
 import { setCalendarAPI } from 'store/calendar/actions'
 import { getFormattedDate } from 'helpers'
@@ -8,37 +8,61 @@ import { connect } from 'react-redux'
 import './Weekly.styl'
 
 class Weekly extends Component {
-  state = {}
+  constructor (props) {
+    super(props)
+    this.state = {
+      visibleDays: this.getVisibleDays(props.defaultDate)
+    }
+  }
 
   componentDidMount = () => {
-    // this.props.dispatch(getEvents())
+    this.view = document.getElementById('calendar-weekly')
+    this.baseCalendarWidth = document.querySelector('.fc.fc-ltr.fc-unthemed').offsetWidth
+    this.view.scrollLeft = this.baseCalendarWidth * 3
+    this.view.addEventListener('scroll', this.updateCalendars)
+  }
+
+  componentWillUnmount () { this.view.removeEventListener('scroll', this.updateCalendars) }
+
+  getVisibleDays = defaultDate => [
+    getFormattedDate(defaultDate, 'subtract', 'days', 8),
+    getFormattedDate(defaultDate, 'subtract', 'days', 4),
+    defaultDate,
+    getFormattedDate(defaultDate, 'add', 'days', 4),
+    getFormattedDate(defaultDate, 'add', 'days', 8)
+  ]
+
+  updateCalendars = () => {
+    const weeklyClientRect = document.querySelector('.calendar-weekly').getBoundingClientRect()
+    if (weeklyClientRect.right < (this.baseCalendarWidth * 2)) {
+      const { visibleDays: vd } = this.state
+      const defaultDate = vd[vd.length - 1]
+      const visibleDays = [ ...vd, ...this.getVisibleDays(getFormattedDate(defaultDate, 'add', 'days', 12)) ].splice(1, 11)
+      this.view.scrollLeft = this.view.scrollLeft - this.baseCalendarWidth
+      this.setState({ visibleDays })
+    } else if (weeklyClientRect.left >= -this.baseCalendarWidth * 2) {
+      this.view.scrollLeft = this.view.scrollLeft + (this.baseCalendarWidth * 5)
+      const defaultDate = getFormattedDate(this.state.visibleDays[0], 'subtract', 'days', 12)
+      const leftDates = this.getVisibleDays(defaultDate)
+      const visibleDays = [ ...leftDates, ...this.state.visibleDays ].splice(0, 9)
+      this.setState({ visibleDays })
+    }
   }
 
   handleEventClick = info => this.setState({ info })
 
   render () {
     const defaultAttr = {
-      eventClick: this.handleEventClick
+      eventClick: this.handleEventClick,
+      refs: node => this.props.dispatch(setCalendarAPI(node, this.props.defaultDate)),
+      events: this.props.events
     }
-
     return (
       <React.Fragment>
         <div id='calendar-weekly'>
-          <div className='calendar-weekly'>
-            <DemoCalendar
-              defaultDate={this.props.defaultDate}
-              events={this.props.events}
-              {...defaultAttr} />
-            <DemoCalendar
-              defaultDate={getFormattedDate(this.props.defaultDate, 'add', 'days', 4)}
-              refs={node => this.props.dispatch(setCalendarAPI(node, this.props.defaultDate))}
-              events={this.props.events}
-              {...defaultAttr} />
-            <DemoCalendar
-              defaultDate={getFormattedDate(this.props.defaultDate, 'add', 'days', 8)}
-              // refs={node => this.props.dispatch(setCalendarAPI(node, this.state.defaultDate))}
-              events={this.props.events}
-              {...defaultAttr} />
+          <div className='calendar-weekly' 
+            style={{ width: this.state.visibleDays.length * 100 + '%' }}>
+            {this.state.visibleDays.map(i => <DemoCalendar key={i} defaultDate={i} {...defaultAttr} />)}
           </div>
         </div>
         <CalendarModal info={this.state.info} handleEventClick={this.handleEventClick} />
