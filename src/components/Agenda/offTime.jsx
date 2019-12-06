4b8aa65836ff8fcc0662ca8ab9775e6855a94cb2
vipslord <vipslord@gmail.com>
2019-12-06 18:32:10 +0200

    fix time label displaying and rendering

---
+++ src/components/Agenda/offTime.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { colorStr, customTimeToText, getHoursLabel, isFreeTimeBoxNeeded } from '../../helpers/event'
+
+const OffTime = (props) => {
+  return (
+    <div className='off_time-wrap'>
+      <div className='off_time' data-id={props.event.id} data-appointment_id={props.event.id}
+           style={{ direction: config.calendar.isRTL ? 'rtl' : 'ltr' }}
+           onClick={() => {props.eventClick(props.event)}}>
+        <div className='off_time_wrap'>
+          <img className='off_time_img'
+               src={`${props.event.off_time === 'break' ? `${config.urls.staticImg}/break.svg` : `${config.urls.staticImg}/meeting.svg`}`}/>
+        </div>
+        <div className='note-info'>
+          <div className={`note-about-user ${config.calendar.isRTL ? ' styleAgendaRTL' : ' styleAgendaLTR'}`}>
+            <p className='name-wrap'>{props.event.off_time === 'break' ? config.translations.business_lunch : config.translations.meeting}</p>
+            {props.event.note
+              ? <div className='note-wrap'>
+                <img className='note-img' src={`${config.urls.staticImg}/feather.svg`}/>
+                <p className='client-note'>{props.event.note}</p>
+              </div>
+              : ''
+            }
+            {props.event.address
+              ? <div className='address-wrap'>
+                <div className='address-img-wrap'>
+                  <img className='address_pin' src={`${config.urls.staticImg}/grey-pin.svg`}/>
+                </div>
+                <p className='address-line'>{props.event.address}</p>
+              </div>
+              : ''
+            }
+          </div>
+          <div className={`time-block ${config.calendar.isRTL ? ' styleTimeRTL' : ' styleTimeLTR'}`}
+               style={{borderImage: `linear-gradient(to bottom ${colorStr(props.event.services)} ) 1 100%`}}
+          >
+            <div className='start-duration'>
+              <p className='event-start'>{getHoursLabel(props.event.start.slice(-5, -3).toString(), props.event.start.slice(-2).toString())}</p>
+              <div className='duration-wrap'>
+                <img className='duration-img' src={`${config.urls.staticImg}/clock.svg`}/>
+                <div className='event-duration'>{customTimeToText(props.event.end, props.event.start)}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {isFreeTimeBoxNeeded(props.events, props.index).isNeeded ? <div className='duration-time' data-time-start={moment(isFreeTimeBoxNeeded(props.events, props.index).end).format('YYYY-MM-DD HH-mm')}>
+        <div className='duration-time-content'>
+          <div className='free-time_value'>{isFreeTimeBoxNeeded(props.events, props.index).hours}</div>
+          <div className='duration-time_container'>
+            <div className='free-time'>
+              <img className='cross' src={`${config.urls.staticImg}/plus.svg`}/>
+            </div>
+          </div>
+          <div className='free-time_title'><span>{config.translations.duration.description}</span></div>
+        </div>
+      </div> : null}
+    </div>
+  );
+}
+
+export default OffTime;
--- src/components/TimeLabels/TimeLabel.jsx
+++ src/components/TimeLabels/TimeLabel.jsx
@@ -6,7 +6,9 @@ export const TimeLabel = (props) => {
   let topParam = config.workers.length === 1 ? 'timeBox-without-workers' : 'timeBox-with-workers'
   return <div className={`timeBox ${topParam}`} style={{
     height: `${getFormattedTimeLabels().length * 25}px`,
-    marginTop: `${props.currentView === 'weekly' ? '23px' : ''}`
+    marginTop: `${props.currentView === 'weekly' ? '23px' : ''}`,
+    left: config.calendar.isRTL ? '' : 0,
+    right: config.calendar.isRTL ? 0 : ''
   }}>
     {getFormattedTimeLabels().map((i, index) => <div key={index}
                                                      className={`time-week${(i !== 0) ? ' true-time-daily' : ''}`}
